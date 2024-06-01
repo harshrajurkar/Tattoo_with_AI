ee77e4cbdd6939709373b417ca51a5e475fd2627
harshrajurkar <harshrajurkar201@gmail.com>
2024-06-01 19:46:04 +0530

    BUILD APP AND REMOVED API

--- lib/Mainscreen.dart
+++ lib/Mainscreen.dart
@@ -26,7 +26,7 @@ class _MainScreenState extends State<MainScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   Future<List<String>> fetchImagesFromUnsplash(String query) async {
-    final String accessKey = '_OtXt-IeY0ZA8Ry4nwvslWbMeRZ8cvjFS_m6Z8yDH7A';
+    final String accessKey = 'ADD_YOUR_API_KEY';
     final String url =
         'https://api.unsplash.com/search/photos?query=$query&client_id=$accessKey';
 
